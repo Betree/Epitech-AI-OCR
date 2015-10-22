a2048e9cd2f86ed579436f3ce5cc590b14aaadbc
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2015-10-22 21:26:44 +0200

    basic opencv demo that displays image specified in argv[1]

--- OCR/sources/main.cpp
+++ OCR/sources/main.cpp
@@ -1,92 +1,27 @@
-//
-// Created by piouffb on 10/4/15.
-//
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
 
-#include <random>
-#include <iostream>
-#include <cmath>
-#include <string>
-#include "NeuralNetwork.hpp"
-#include "Trainer.hpp"
+using namespace cv;
 
-using namespace nn;
-using namespace std;
-
-int main()
+int main(int argc, char** argv )
 {
-    NeuralNetwork network(8, 2, { 4 });
-	Trainer trainer(&network);
-
-	// Generate test epoch.
-	const unsigned int epochSize = 1000;
-	vector<Trainer::InputOutputPair> epoch;
-	epoch.reserve(epochSize);
-	default_random_engine generator;
-	uniform_int_distribution<int> distribution(numeric_limits<char>::min(), numeric_limits<char>::max());
-	for (size_t i = 0; i < epochSize; i++)
+	if (argc != 2)
 	{
-		unsigned char cInput = distribution(generator);
-		vector<double> feedingData;
-
-		// Decompose char into 8 boules
-		for (int cBinaryComparator = 128; cBinaryComparator > 0; cBinaryComparator /= 2)
-		{
-			feedingData.push_back((cInput & cBinaryComparator) != 0);
-		}
-		epoch.push_back(Trainer::InputOutputPair(feedingData, { (double)((cInput % 2) == 0), (double)((cInput % 2) != 0) }));
+		printf("usage: DisplayImage.out <Image_Path>\n");
+		return -1;
 	}
 
-	string line;
-	while (cin)
+	Mat image;
+	image = imread(argv[1], 1);
+
+	if (!image.data)
 	{
-		char cInput;
-		int value;
-
-		cout << "Feed value: ";
-		if (cin >> value)
-		{
-			cInput = (char)value;
-			cin.clear();
-			cin.ignore((streamsize)numeric_limits<streamsize>::max, '\n');
-
-			vector<double> feedingData;
-			// Decompose char into 8 boules
-			for (int cBinaryComparator = 128; cBinaryComparator > 0; cBinaryComparator /= 2)
-			{
-				feedingData.push_back((cInput & cBinaryComparator) != 0);
-			}
-
-			vector<double> updateResult = network.update(feedingData);
-			cout << "Testing " << (int)cInput << endl;
-			cout << "Pair probability : " << updateResult[0] << endl;
-			cout << "Impair probability : " << updateResult[1] << endl;
-			cout << "----------------------------------------" << endl;
-
-			cout << "Press return to feed an epoch or Ctrl+D to stop...";
-			if (getline(cin, line))
-			{
-				trainer.train(epoch);
-			}
-		}
+		printf("No image data \n");
+		return -1;
 	}
+	namedWindow("Display Image", WINDOW_AUTOSIZE );
+	imshow("Display Image", image);
+	waitKey(0);
+	
 	return 0;
-
-    // Feed network with values between 0 and 20
- //   for (unsigned char cInput = 1; cInput < 20; ++cInput)
- //   {
- //       std::vector<double> feedingData;
-
- //       // Decompose char into 8 boules
- //       for (int cBinaryComparator = 128; cBinaryComparator > 0; cBinaryComparator /= 2)
- //       {
- //           feedingData.push_back((cInput & cBinaryComparator) != 0);
- //       }
-
- //       std::vector<double> updateResult = network.update(feedingData);
- //       std::cout << "Testing " << (int)cInput << std::endl;
- //       std::cout << "Pair probability : " << updateResult[0] << std::endl;
- //       std::cout << "Impair probability : " << updateResult[1] << std::endl;
- //       std::cout << "----------------------------------------" << std::endl;
- //   }
-	//std::cin.get();
 }
