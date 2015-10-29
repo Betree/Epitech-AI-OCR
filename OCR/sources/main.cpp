212876df96be34c3fce34089b74319cbb7803908
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2015-10-29 19:25:55 +0100

    delete useless comment

--- OCR/sources/main.cpp
+++ OCR/sources/main.cpp
@@ -7,24 +7,6 @@ using namespace cv;
 
 int main(int argc, char** argv)
 {
-/*	if (argc != 2)
-	{
-		printf("usage: OCR <Image_Path>\n");
-		return -1;
-	}
-
-	Mat image;
-	image = imread(argv[1], 1);
-	if (!image.data)
-	{
-		printf("No image data \n");
-		return -1;
-	}
-
-	namedWindow("Display Image", WINDOW_AUTOSIZE);
-	imshow("Display Image", image);
-	waitKey(0);
-*/
 	ImagesLoader loader("/home/piouffb/workspace/EPITECH/Epitech-AI-OCR/OCR/data/55x_small_dataset/55x_small_dataset");
 	ImageProcessor processor;
 	Mat image;
